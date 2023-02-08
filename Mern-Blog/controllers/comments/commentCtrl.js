9807a69484c834ddd04ae45917272347a10337c6
Paing Soe Oo <mgpaingsoe.ps@gmail.com>
2023-02-08 12:00:00 +0630

    Comment CRUD

--- Mern-Blog/controllers/comments/commentCtrl.js
+++ Mern-Blog/controllers/comments/commentCtrl.js
@@ -1,7 +1,8 @@
 const expressAsyncHandler = require("express-async-handler");
 const Comment = require("../../model/comment/comment");
+const validateMongodbId = require("../../utils/validateMongodbID");
 
-
+//Create
 const createCommentCtrl = expressAsyncHandler(async (req, res) => {
   console.log(req.user);
 
@@ -24,6 +25,63 @@ const createCommentCtrl = expressAsyncHandler(async (req, res) => {
   }
 });
 
+//Fetch all comments
+const fetchAllCommentsCtrl = expressAsyncHandler(async (req, res) => {
+  try {
+    const comments = await Comment.find({}).sort("-created");
+    res.json(comments);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//Comment details
+const fetchCommentCtrl = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongodbId(id);
+
+  try {
+    const comment = await Comment.findById(id);
+    res.json(comment);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//Update
+const updateCommentCtrl = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongodbId(id);
+
+  try {
+    const update = await Comment.findByIdAndUpdate(id, {
+      post: req.body?.postId,
+      user: req?.user,
+      description: req?.body?.description,
+    }, { new: true, runValidators: true});
+    res.json(update);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//Delete
+const deleteCommentCtrl = expressAsyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongodbId(id);
+
+  try {
+    const comment = await Comment.findByIdAndDelete(id);
+    res.json(comment);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
 module.exports = {
   createCommentCtrl,
+  fetchAllCommentsCtrl,
+  fetchCommentCtrl,
+  updateCommentCtrl,
+  deleteCommentCtrl
 }
--- Mern-Blog/route/comments/commentRoute.js
+++ Mern-Blog/route/comments/commentRoute.js
@@ -1,12 +1,20 @@
 const express = require("express");
 
 const { 
-  createCommentCtrl 
+  createCommentCtrl,
+  fetchAllCommentsCtrl,
+  fetchCommentCtrl,
+  deleteCommentCtrl,
+  updateCommentCtrl
 } = require("../../controllers/comments/commentCtrl");
 const authMiddleware = require("../../middlewares/auth/authMiddleware");
 
 const commentRouter = express.Router();
 
 commentRouter.post("/", authMiddleware, createCommentCtrl);
+commentRouter.get("/", authMiddleware, fetchAllCommentsCtrl);
+commentRouter.get("/:id", authMiddleware, fetchCommentCtrl);
+commentRouter.put("/:id", authMiddleware, updateCommentCtrl);
+commentRouter.delete("/:id", authMiddleware, deleteCommentCtrl);
 
 module.exports = commentRouter;
